852b66aff0f79ba334a90b364a587546f98de1dc
Your Name <you@example.com>
2021-08-08 13:57:26 +0900

    [update]oledのopen時に初期化コマンド送付するよう実装

--- src/nanohat/oled.rs
+++ src/nanohat/oled.rs
@@ -7,6 +7,36 @@ pub struct NanoHatOLED{
 impl NanoHatOLED {
     pub fn open() -> Result<Self, LinuxI2CError> {
         let mut dev = LinuxI2CDevice::new("/dev/i2c-0", 0x3c)?;
+        let commands = [
+            0xAE, //display off
+            0x00, //set lower column address
+            0x10, //set higher column address
+            0x40, //set display start line
+            0xB0, //set page address
+            0x81, //set contrast control
+            0xCF,
+            0xA1, //set segment re-map
+            0xA6, //set normal display
+            0xA8, //set multiplex ratio
+            0x3F,
+            0xC8, //Set COM OutputScan Direction
+            0xD3, //set display offset
+            0x00,
+            0xD5, //set display clock divide ratio/ oscillator frequency
+            0x80,
+            0xD9, //set pre-charge period
+            0xF1,
+            0xDA, //set COM pins
+            0x12, 
+            0xDB, //set vcomh
+            0x40,
+            0x8D, //set charge pump enable
+            0x14,
+            0x20, //set horizontal mode
+            0x00,
+            0xAF, //display on
+        ];
+        Self::send_commands(&mut dev, &commands)?;
         Ok(
             Self {
                 dev,
