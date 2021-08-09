bc6e0ca6dc73b888435ab8174cd062c0971a41a2
Your Name <you@example.com>
2021-08-09 12:31:56 +0900

    [add]draw_image実装
    draw_image実装とそれに伴うテスト実装

--- src/nanohat/oled.rs
+++ src/nanohat/oled.rs
@@ -1,8 +1,13 @@
 use i2cdev::core::I2CDevice;
+use image::imageops::{dither, BiLevel};
+use image::{DynamicImage, GenericImageView};
+use std::error::Error;
+use std::io;
 
 pub struct NanoHatOLED<T>
 where
     T: I2CDevice + Sized,
+    T::Error: 'static,
 {
     i2cdev: T,
 }
@@ -34,26 +39,115 @@ where
         Self::send_commands(&mut i2cdev, &commands)?;
         Ok(Self { i2cdev })
     }
-    fn send_commands(dev: &mut T, commands: &[u8]) -> Result<(), T::Error> {
-        dev.smbus_write_i2c_block_data(0x00, commands)?;
+    fn send_commands(i2cdev: &mut T, commands: &[u8]) -> Result<(), T::Error> {
+        i2cdev.smbus_write_i2c_block_data(0x00, commands)?;
         Ok(())
     }
-    fn send_data(dev: &mut T, data: &[u8]) -> Result<(), T::Error> {
+    fn send_data(i2cdev: &mut T, data: &[u8]) -> Result<(), T::Error> {
         for chunk in data.chunks(32) {
-            dev.smbus_write_i2c_block_data(0x40, chunk)?;
+            i2cdev.smbus_write_i2c_block_data(0x40, chunk)?;
         }
+        Ok(())
+    }
+
+    pub fn set_draw_range(&mut self, x: u8, y: u8, w: u8, h: u8) -> Result<(), T::Error> {
+        let commands = [0x21, x, x + w - 1, 0x22, y, y + h - 1];
+        Self::send_commands(&mut self.i2cdev, &commands)?;
+        Ok(())
+    }
+
+    pub fn reset_draw_range(&mut self) -> Result<(), T::Error> {
+        self.set_draw_range(0, 0, 128, 8)?;
+        Ok(())
+    }
+
+    pub fn draw_image(&mut self, img: &DynamicImage, x: u8, y: u8) -> Result<(), Box<dyn Error>> {
+        let w = img.width();
+        let (h, rem) = {
+            let h = img.height();
+            (h / 8, h % 8)
+        };
+
+        if x as u32 + w > 128 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "The Image's dimensions are too large",
+            )
+            .into());
+        } else if rem != 0 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "This Image's height are invalid ",
+            )
+            .into());
+        } else if y as u32 + h > 8 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "The Image's dimensions are too large",
+            )
+            .into());
+        } else {
+            /*do nothing*/
+        }
+
+        let mut gray_img = img.grayscale().into_luma8();
+        dither(&mut gray_img, &BiLevel);
+
+        let height_pixel_offsets = [0, 8, 16, 24, 32, 40, 48, 56];
+        let data = height_pixel_offsets
+            .iter()
+            .map(|offset| {
+                (0..w)
+                    .map(|x| {
+                        (0..8).rev().fold(0u8, |mut bits, y| {
+                            let px = gray_img.get_pixel(x as u32, offset + y);
+                            bits <<= 1;
+                            bits |= if px[0] == 255 { 0 } else { 1 };
+                            bits
+                        })
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .flat_map(|v| v)
+            .collect::<Vec<_>>();
+        Self::send_data(&mut self.i2cdev, &data)?;
         Ok(())
     }
 }
 
+#[cfg(target_os = "linux")]
 #[cfg(test)]
 mod tests {
     use super::*;
+    use i2cdev::linux::LinuxI2CDevice;
     #[test]
-    #[cfg(target_os = "linux")]
     fn staging_oled_open_test() {
-        use i2cdev::linux::LinuxI2CDevice;
         let i2cdev = LinuxI2CDevice::new("/dev/i2c-0", 0x3c).unwrap();
         assert!(NanoHatOLED::open(i2cdev).is_ok());
+    }
+
+    #[test]
+    fn draw_image_test() {
+        let i2cdev = LinuxI2CDevice::new("/dev/i2c-0", 0x3c).unwrap();
+        let mut oled = NanoHatOLED::open(i2cdev).unwrap();
+
+        let img_128x64 = DynamicImage::new_luma8(128, 64);
+        assert!(oled.draw_image(&img_128x64, 0, 0).is_ok());
+        assert!(oled.draw_image(&img_128x64, 1, 0).is_err());
+        assert!(oled.draw_image(&img_128x64, 0, 1).is_err());
+        assert!(oled.draw_image(&img_128x64, 128, 0).is_err());
+        assert!(oled.draw_image(&img_128x64, 0, 8).is_err());
+
+        let img_8x8 = DynamicImage::new_luma8(8, 8);
+        assert!(oled.draw_image(&img_8x8, 120, 0).is_ok());
+        assert!(oled.draw_image(&img_8x8, 120, 7).is_ok());
+        assert!(oled.draw_image(&img_8x8, 0, 0).is_ok());
+        assert!(oled.draw_image(&img_8x8, 0, 7).is_ok());
+
+        let img_8x9 = DynamicImage::new_luma8(8, 9);
+        assert!(oled.draw_image(&img_8x8, 0, 0).is_err());
+
+        let img_10x8 = DynamicImage::new_luma8(10, 8);
+        assert!(oled.draw_image(&img_8x8, 0, 0).is_ok());
     }
 }
