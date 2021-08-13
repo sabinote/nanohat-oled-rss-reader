7b3e89c37ddb0deafd0473555934916f16793d12
Your Name <you@example.com>
2021-08-13 13:23:52 +0900

    [add]reqwestを使ってURLからXMLを取得するコードを実装

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod nanohat;
 mod rss;
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    let xml = reqwest::get("https://news.yahoo.co.jp/rss/topics/top-picks.xml")
+        .await?
+        .test()
+        .await?;
+
+    let rss = rss::RSS::from_str(s)?;
+    println!("{}", rss.channel.title);
 }
