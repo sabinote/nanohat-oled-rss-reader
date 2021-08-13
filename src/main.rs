804a5495aab7c2ae7a5729b516f32ca36145295c
Your Name <you@example.com>
2021-08-13 13:27:06 +0900

    [fix]変数名修正

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod nanohat;
 mod rss;
 
+use std::error::Error
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let xml = reqwest::get("https://news.yahoo.co.jp/rss/topics/top-picks.xml")
         .await?
         .test()
         .await?;
 
-    let rss = rss::RSS::from_str(s)?;
+    let rss = rss::RSS::from_str(&xml)?;
     println!("{}", rss.channel.title);
+    Ok(())
 }
