22ea7cb460dc032e26519bab1c4fda7fc5eff77c
Your Name <you@example.com>
2021-08-14 21:42:12 +0900

    [add]上スクロールを実装

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 mod nanohat;
 mod rss;
 
+use i2cdev::linux::LinuxI2CDevice;
+use image::imageops::colorops::invert;
+use image::{DynamicImage, GenericImage, GrayImage, Luma};
+use imageproc::drawing::draw_text_mut;
+use rusttype::{Font, Scale};
 use std::error::Error;
 use std::ops::Range;
-use rusttype::{Font, Scale};
-use image::{DynamicImage, GrayImage, Luma, GenericImage};
-use image::imageops::colorops::invert;
-use imageproc::drawing::draw_text_mut;
-use i2cdev::linux::LinuxI2CDevice;
-
 
 struct CategoryPane {
     categories: Vec<(String, String)>,
@@ -22,28 +21,54 @@ enum State {
     Details,
 }
 
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let i2cdev = LinuxI2CDevice::new("/dev/i2c-0", 0x3c)?;
     let mut oled = nanohat::oled::NanoHatOLED::open(i2cdev)?;
     let mut button = nanohat::button::Button::open("/dev/gpiochip0")?;
     let font = {
-        let v =  Vec::from(include_bytes!("font/misaki_gothic.ttf") as &[u8]);
+        let v = Vec::from(include_bytes!("font/misaki_gothic.ttf") as &[u8]);
         Font::try_from_vec(v).unwrap()
     };
 
     let mut category_pane = CategoryPane {
         categories: vec![
-            ("主要".to_string(),"https://news.yahoo.co.jp/rss/topics/top-picks.xml".to_string()),
-            ("国内".to_string(),"https://news.yahoo.co.jp/rss/topics/domestic.xml".to_string()),
-            ("国際".to_string(),"https://news.yahoo.co.jp/rss/topics/world.xml".to_string()),
-            ("経済".to_string(),"https://news.yahoo.co.jp/rss/topics/business.xml".to_string()),
-            ("エンタメ".to_string(),"https://news.yahoo.co.jp/rss/topics/entertainment.xml".to_string()),
-            ("スポーツ".to_string(),"https://news.yahoo.co.jp/rss/topics/sports.xml".to_string()),
-            ("IT".to_string(),"https://news.yahoo.co.jp/rss/topics/it.xml".to_string()),
-            ("科学".to_string(),"https://news.yahoo.co.jp/rss/topics/science.xml".to_string()),
-            ("地域".to_string(),"https://news.yahoo.co.jp/rss/topics/local.xml".to_string()),
+            (
+                "主要".to_string(),
+                "https://news.yahoo.co.jp/rss/topics/top-picks.xml".to_string(),
+            ),
+            (
+                "国内".to_string(),
+                "https://news.yahoo.co.jp/rss/topics/domestic.xml".to_string(),
+            ),
+            (
+                "国際".to_string(),
+                "https://news.yahoo.co.jp/rss/topics/world.xml".to_string(),
+            ),
+            (
+                "経済".to_string(),
+                "https://news.yahoo.co.jp/rss/topics/business.xml".to_string(),
+            ),
+            (
+                "エンタメ".to_string(),
+                "https://news.yahoo.co.jp/rss/topics/entertainment.xml".to_string(),
+            ),
+            (
+                "スポーツ".to_string(),
+                "https://news.yahoo.co.jp/rss/topics/sports.xml".to_string(),
+            ),
+            (
+                "IT".to_string(),
+                "https://news.yahoo.co.jp/rss/topics/it.xml".to_string(),
+            ),
+            (
+                "科学".to_string(),
+                "https://news.yahoo.co.jp/rss/topics/science.xml".to_string(),
+            ),
+            (
+                "地域".to_string(),
+                "https://news.yahoo.co.jp/rss/topics/local.xml".to_string(),
+            ),
         ],
         display_range: 0..8,
         selected: 0,
@@ -51,18 +76,36 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut state = State::Categories;
 
     let mut img = GrayImage::new(128, 64);
-    for (i, (s, _)) in category_pane.categories[category_pane.display_range.start..category_pane.display_range.end].iter().enumerate() {
+    for (i, (s, _)) in category_pane.categories
+        [category_pane.display_range.start..category_pane.display_range.end]
+        .iter()
+        .enumerate()
+    {
         if category_pane.selected == i {
             let mut sub = img.sub_image(0, (i * 8) as u32, 128, 8);
             invert(&mut sub);
-            draw_text_mut(&mut img, Luma([0]), 0, (i * 8) as u32, Scale{x:8.0, y:8.0}, &font, s);
-        }
-        else {
-            draw_text_mut(&mut img, Luma([255]), 0, (i * 8) as u32, Scale{x:8.0, y:8.0}, &font, s);
+            draw_text_mut(
+                &mut img,
+                Luma([0]),
+                0,
+                (i * 8) as u32,
+                Scale { x: 8.0, y: 8.0 },
+                &font,
+                s,
+            );
+        } else {
+            draw_text_mut(
+                &mut img,
+                Luma([255]),
+                0,
+                (i * 8) as u32,
+                Scale { x: 8.0, y: 8.0 },
+                &font,
+                s,
+            );
         }
     }
     oled.draw_image(&DynamicImage::ImageLuma8(img), 0, 0)?;
-
 
     while let Ok(pressed) = button.pressed().await {
         match state {
@@ -72,59 +115,155 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         if category_pane.selected < 7 {
                             let mut img = GrayImage::new(128, 8);
                             let i = category_pane.display_range.start + category_pane.selected;
-                            draw_text_mut(&mut img, Luma([255]), 0, 0, Scale{x:8.0, y:8.0}, &font, &category_pane.categories[i].0);
+                            draw_text_mut(
+                                &mut img,
+                                Luma([255]),
+                                0,
+                                0,
+                                Scale { x: 8.0, y: 8.0 },
+                                &font,
+                                &category_pane.categories[i].0,
+                            );
                             //oled.set_draw_range(0, category_pane.selected as u8, 128, 8)?;
-                            oled.draw_image(&DynamicImage::ImageLuma8(img), 0, category_pane.selected as u8)?;
+                            oled.draw_image(
+                                &DynamicImage::ImageLuma8(img),
+                                0,
+                                category_pane.selected as u8,
+                            )?;
 
                             let mut img = GrayImage::new(128, 8);
                             invert(&mut img);
                             category_pane.selected += 1;
                             let i = category_pane.display_range.start + category_pane.selected;
-                            draw_text_mut(&mut img, Luma([0]), 0, 0, Scale{x:8.0, y:8.0}, &font, &category_pane.categories[i].0);
+                            draw_text_mut(
+                                &mut img,
+                                Luma([0]),
+                                0,
+                                0,
+                                Scale { x: 8.0, y: 8.0 },
+                                &font,
+                                &category_pane.categories[i].0,
+                            );
                             //oled.set_draw_range(0, category_pane.selected as u8, 128, 8)?;
-                            oled.draw_image(&DynamicImage::ImageLuma8(img), 0, category_pane.selected as u8)?;
-                        }
-                        else if category_pane.display_range.end < category_pane.categories.len() {
+                            oled.draw_image(
+                                &DynamicImage::ImageLuma8(img),
+                                0,
+                                category_pane.selected as u8,
+                            )?;
+                        } else if category_pane.display_range.end < category_pane.categories.len() {
                             let mut img = GrayImage::new(128, 64);
                             let start = category_pane.display_range.start + 1;
                             let end = category_pane.display_range.end + 1;
                             category_pane.display_range = start..end;
                             let i = 7;
                             category_pane.selected = 7;
-                            for (i, (s, _)) in category_pane.categories[start..end].iter().enumerate() {
+                            for (i, (s, _)) in
+                                category_pane.categories[start..end].iter().enumerate()
+                            {
                                 if category_pane.selected == i {
                                     let mut sub = img.sub_image(0, (i * 8) as u32, 128, 8);
                                     invert(&mut sub);
-                                    draw_text_mut(&mut img, Luma([0]), 0, (i * 8) as u32, Scale{x:8.0, y:8.0}, &font, s);
-                                }
-                                else {
-                                    draw_text_mut(&mut img, Luma([255]), 0, (i * 8) as u32, Scale{x:8.0, y:8.0}, &font, s);
+                                    draw_text_mut(
+                                        &mut img,
+                                        Luma([0]),
+                                        0,
+                                        (i * 8) as u32,
+                                        Scale { x: 8.0, y: 8.0 },
+                                        &font,
+                                        s,
+                                    );
+                                } else {
+                                    draw_text_mut(
+                                        &mut img,
+                                        Luma([255]),
+                                        0,
+                                        (i * 8) as u32,
+                                        Scale { x: 8.0, y: 8.0 },
+                                        &font,
+                                        s,
+                                    );
                                 }
                             }
                             oled.draw_image(&DynamicImage::ImageLuma8(img), 0, 0)?;
-
+                        } else {
                         }
-                    },
+                    }
                     [false, false, true] => {
                         if category_pane.selected > 0 {
                             let mut img = GrayImage::new(128, 8);
                             let i = category_pane.display_range.start + category_pane.selected;
-                            draw_text_mut(&mut img, Luma([255]), 0, 0, Scale{x:8.0, y:8.0}, &font, &category_pane.categories[i].0);
-                            oled.draw_image(&DynamicImage::ImageLuma8(img), 0, category_pane.selected as u8)?;
+                            draw_text_mut(
+                                &mut img,
+                                Luma([255]),
+                                0,
+                                0,
+                                Scale { x: 8.0, y: 8.0 },
+                                &font,
+                                &category_pane.categories[i].0,
+                            );
+                            oled.draw_image(
+                                &DynamicImage::ImageLuma8(img),
+                                0,
+                                category_pane.selected as u8,
+                            )?;
 
                             let mut img = GrayImage::new(128, 8);
                             invert(&mut img);
                             category_pane.selected -= 1;
                             let i = category_pane.display_range.start + category_pane.selected;
-                            draw_text_mut(&mut img, Luma([0]), 0, 0, Scale{x:8.0, y:8.0}, &font, &category_pane.categories[i].0);
-                            oled.draw_image(&DynamicImage::ImageLuma8(img), 0, category_pane.selected as u8)?;
+                            draw_text_mut(
+                                &mut img,
+                                Luma([0]),
+                                0,
+                                0,
+                                Scale { x: 8.0, y: 8.0 },
+                                &font,
+                                &category_pane.categories[i].0,
+                            );
+                            oled.draw_image(
+                                &DynamicImage::ImageLuma8(img),
+                                0,
+                                category_pane.selected as u8,
+                            )?;
+                        } else if category_pane.display_range.start > 0 {
+                            let mut img = GrayImage::new(128, 64);
+                            let start = category_pane.display_range.start - 1;
+                            let end = category_pane.display_range.end - 1;
+                            let i = 0;
+                            category_pane.selected = 0;
+                            for (i, (s, _)) in
+                                category_pane.categories[start..end].iter().enumerate()
+                            {
+                                if category_pane.selected == i {
+                                    let mut sub = img.sub_image(0, (i * 8) as u32, 128, 8);
+                                    invert(&mut sub);
+                                    draw_text_mut(
+                                        &mut img,
+                                        Luma([0]),
+                                        0,
+                                        (i * 8) as u32,
+                                        Scale { x: 8.0, y: 8.0 },
+                                        &font,
+                                        s,
+                                    );
+                                } else {
+                                    draw_text_mut(
+                                        &mut img,
+                                        Luma([255]),
+                                        0,
+                                        (i * 8) as u32,
+                                        Scale { x: 8.0, y: 8.0 },
+                                        &font,
+                                        s,
+                                    );
+                                }
+                            }
+                            oled.draw_image(&DynamicImage::ImageLuma8(img), 0, 0)?;
                         }
-                    },
-                    _ => {
-                        ()
                     }
+                    _ => (),
                 }
-            },
+            }
             _ => unimplemented!(),
         }
     }
