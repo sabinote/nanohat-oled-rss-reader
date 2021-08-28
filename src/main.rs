35700f1e2a1bd8926ea2a1fefc5b244c5271a1bd
Your Name <you@example.com>
2021-08-28 11:32:30 +0900

    [fix]コンパイルエラー修正

--- src/main.rs
+++ src/main.rs
@@ -326,7 +326,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 [false, true, false] => {
                     let i = title_pane.start_i + title_pane.selected;
                     let s = match title_pane.descriptions.get(i).unwrap() {
-                        Some(ref s) => s,
+                        Some(s) => &s,
                         None => "Not found",
                     };
 
